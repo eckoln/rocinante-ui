e88260be077536759871a4f53e7781e941be4d41
Ahmet <rganned@gmail.com>
2022-11-27 19:20:11 +0300

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,16 +2,27 @@ import * as class_variance_authority_dist_types from 'class-variance-authority/d
 import React from 'react';
 import { VariantProps } from 'class-variance-authority';
 
-declare const button: (props?: ({
-    intent?: null | undefined;
-    size?: null | undefined;
+declare const buttonStyles: (props?: ({
+    intent?: "primary" | "secondary" | "danger" | "ghost" | null | undefined;
+    size?: "sm" | null | undefined;
 } & class_variance_authority_dist_types.ClassProp) | undefined) => string;
 type ButtonProps = {
     startIcon?: React.FC<React.ComponentProps<"svg">>;
     endIcon?: React.FC<React.ComponentProps<"svg">>;
 };
-interface Props extends ButtonProps, React.ComponentProps<"button">, VariantProps<typeof button> {
+interface Props$1 extends ButtonProps, React.ComponentProps<"button">, VariantProps<typeof buttonStyles> {
 }
-declare function Button({ intent, size, startIcon, endIcon, className, children, ...props }: Props): JSX.Element;
+declare function Button({ intent, size, startIcon, endIcon, className, children, ...props }: Props$1): JSX.Element;
 
-export { Button };
+declare const headingStyles: (props?: ({
+    variant?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | null | undefined;
+    defaultVariants?: "variant" | null | undefined;
+} & class_variance_authority_dist_types.ClassProp) | undefined) => string;
+type HeadingProps = {
+    as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+};
+interface Props extends HeadingProps, React.ComponentProps<"h1">, VariantProps<typeof headingStyles> {
+}
+declare function Heading({ variant, as, className, children, ...props }: Props): JSX.Element;
+
+export { Button, Heading };
